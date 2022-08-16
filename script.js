7e17bd7585787c79b6a849c0e398f4a108bf1092
ArunasMark <107833251+ArunasMark@users.noreply.github.com>
2022-08-16 16:07:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+let today = new Date();
+
+document.getElementById("today").innerHTML = today.toDateString();
+
+let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
+document.getElementById("weekday").innerHTML = day;
+
+let inputval = document.querySelector("#cityinput");
+let btn = document.querySelector("#add");
+let city = document.querySelector("#cityoutput");
+let descrip = document.querySelector("#description");
+let temp = document.querySelector("#temp");
+let wind = document.querySelector("#wind");
+apik = "3045dd712ffe6e702e3245525ac7fa38";
+function convertion(val) {
+  return (val - 273).toFixed(0);
+}
+
+btn.addEventListener("click", function (event) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    inputval.value +
+    "&appid=" +
+    apik
+  )
+    .then((res) => res.json())
+
+    .then((data) => {
+      let nameval = data["name"];
+      let descrip = data["weather"]["0"]["description"];
+      let tempature = data["main"]["temp"];
+      let wndspd = data["wind"]["speed"];
+      city.innerHTML = `Weather of <span>${nameval}.<span>`;
+      temp.innerHTML = `Temperature: <span>${convertion(tempature)} °C.</span>`;
+      description.innerHTML = `Sky Conditions: <span>${descrip}.<span>`;
+      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h.<span>`;
+    })
+
+    .catch((err) => alert("You entered WRONG city name !!!"));
+  
+});
