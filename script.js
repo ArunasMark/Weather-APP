c840f18deee55b6cde7d87f5a7f5af0acd9015b3
ArunasMark <107833251+ArunasMark@users.noreply.github.com>
2023-02-27 14:38:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -33,9 +33,13 @@ let city = document.querySelector("#cityoutput");
 let descrip = document.querySelector("#description");
 let temp = document.querySelector("#temp");
 let wind = document.querySelector("#wind");
+
 apik = "3045dd712ffe6e702e3245525ac7fa38";
 function convertion(val) {
-  return (val - 273).toFixed(0);
+  return (val - 273).toFixed();
+}
+function kmph_to_mps(val) {
+  return (0.277778 * val).toFixed(1);
 }
 
 btn.addEventListener("click", function (event) {
@@ -49,15 +53,25 @@ btn.addEventListener("click", function (event) {
 
     .then((data) => {
       let nameval = data["name"];
+      let country = data["sys"]["country"];
       let descrip = data["weather"]["0"]["description"];
       let tempature = data["main"]["temp"];
       let wndspd = data["wind"]["speed"];
-      city.innerHTML = `Weather of <span>${nameval}.<span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} °C.</span>`;
-      description.innerHTML = `Sky Conditions: <span>${descrip}.<span>`;
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h.<span>`;
+      let hum = data["main"]['humidity'];
+      let feelTemp = data["main"]["feels_like"];
+      
+      
+      console.log(data);
+      city.innerHTML = `Weather of <span>${nameval}, (${country})</span>`;
+      temp.innerHTML = `Temperature: <span>${convertion(
+        tempature
+      )} °C,</span> feels like: <span>${convertion(feelTemp)} °C.</span>`;
+      description.innerHTML = `Sky Conditions: <span>${descrip}.</span>`;
+      wind.innerHTML = `Wind Speed: <span>${kmph_to_mps(wndspd)} m/s, (${wndspd} km/h).<span>`;
+      humidity.innerHTML = `Humidity: <span>${hum}  %.</span>`;
+      
     })
-
-    .catch((err) => alert("You entered WRONG city name !!!"));
+    
+    .catch((err) => alert("Neįvestas (arba blogai įvestas) miesto pavadinimas !!!"));
   
 });
